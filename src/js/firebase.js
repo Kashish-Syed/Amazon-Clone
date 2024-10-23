451fa526c0a0dae78c2a8e5cde803af716ce9c28
Kashish Syed <111908311+Kashish-Syed@users.noreply.github.com>
2024-10-23 11:41:40 -0500

    Update firebase.js to hide secret keys

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -3,13 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCgmvujrv-YAGP0mp2hoPL_G7kqs2ZEZag",
-    authDomain: "clone-72e22.firebaseapp.com",
-    projectId: "clone-72e22",
-    storageBucket: "clone-72e22.appspot.com",
-    messagingSenderId: "406616709165",
-    appId: "1:406616709165:web:550a94be1221f30daec0a4",
-    measurementId: "G-ETF4ZS6MCQ"
+    // ADD FIREBASE CONFIG FILE HERE
   };
 
 const firebaseApp = initializeApp(firebaseConfig);
